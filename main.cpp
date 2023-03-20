03582f9a5dfba5f9043f466a2987aca8e0400751
Ivan Yatsenko <aokiroyal16@gmail.com>
2023-03-20 17:21:51 +0200

    Added welcome window (shows controls)

--- main.cpp
+++ main.cpp
@@ -1,6 +1,15 @@
 ﻿#include "Cases.h"
+
+void welcome() {
+    std::cout << "W - to move up\n";
+    std::cout << "S - to move down\n";
+    std::cout << "SPACE to choose\n\n";
+    system("pause");
+}
+
 int main()
 {
+    welcome();
     Manager manager;
     manager.load();
     manager.menu();
